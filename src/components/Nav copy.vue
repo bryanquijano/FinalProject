aabee2ce44ce3b05fa1d982cc328ea6e2fe036a4
Bryan <bryanoquijano@gmail.com>
2022-03-02 21:18:52 -0600

    v0.0.2 : Started design on the register page

--- src/components/Nav copy.vue
+++ src/components/Nav copy.vue
@@ -23,52 +23,51 @@ const loggingOut = () => {
       </router-link>
       <nav>
         <ul class="flex space-x-4">
+          <!-- Static method -->
+          <router-link to="/">
+            <li
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
+            >
+              Home
+            </li>
+          </router-link>
+          <!-- Dynamic method -->
+          <router-link :to="{ name: 'Members' }">
+            <li
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
+            >
+              Members
+            </li>
+          </router-link>
+          <router-link :to="{ name: 'Music' }">
+            <li
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
+            >
+              Music
+            </li>
+          </router-link>
+          <router-link :to="{ name: 'Shop' }">
+            <li
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
+            >
+              Shop
+            </li>
+          </router-link>
           <router-link v-if="!isAuthenticated" :to="{ name: 'Login' }">
             <li
-              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-300 tracking-tight font-bold"
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
             >
-              LOG IN
+              Login
             </li>
           </router-link>
           <router-link v-if="!isAuthenticated" :to="{ name: 'Register' }">
             <li
-              class="py-2 px-6 mt-6 hover:cursor-pointer text-white hover:text-gray-300 tracking-tight font-bold bg-red-500 rounded-full hover:bg-red-800"
+              class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
             >
-              REGISTER
+              Register
             </li>
           </router-link>
-          <!-- Static method -->
           <div v-else class="flex">
-            <router-link to="/">
-              <li
-                class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
-              >
-                Home
-              </li>
-            </router-link>
-            <!-- Dynamic method -->
-            <router-link :to="{ name: 'Members' }">
-              <li
-                class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
-              >
-                Members
-              </li>
-            </router-link>
-            <router-link :to="{ name: 'Music' }">
-              <li
-                class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
-              >
-                Music
-              </li>
-            </router-link>
-            <router-link :to="{ name: 'Shop' }">
-              <li
-                class="py-8 px-4 hover:cursor-pointer text-white hover:text-gray-700 hover:bg-gray-300"
-              >
-                Shop
-              </li>
-            </router-link>
-
             <router-link :to="{ name: 'Fanclub' }">
               <li
                 class="py-8 px-4 hover:cursor-pointer hover:bg-gray-300 hover:text-gray-700"
