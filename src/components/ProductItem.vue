2b7a382b41f21f5cf5987a31f15e6693c49b2824
Bryan <11167417+bryanquijano@users.noreply.github.com>
2022-04-22 08:07:49 -0500

    Refactored shop page

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -11,23 +11,28 @@ onUnmounted(() => {
 
 <template>
   <div v-for="product in products" :key="product.id">
-    <router-link :to="{ name: `${product.ID}` }">
-      <div
-        class="bg-gray-800 w-56 h-76 hover:opacity-80 transition ease-in-out duration-500 p-4 rounded-md pb-6"
+    <div
+      class="w-64 h-76 transition ease-in-out duration-500 p-4 rounded-md pb-6"
+    >
+      <img
+        :src="`${product.Img}`"
+        alt=""
+        class="w-56 h-56 mx-auto bg-gray-300 shadow-2xl"
+      />
+      <h2 class="font-bold text-xl pt-2 truncate ...">{{ product.Name }}</h2>
+      <h4 class="text-gray-400">${{ product.Price }}</h4>
+      <button
+        v-if="product.SoldOut == false"
+        class="text-center w-full bg-blue-600 rounded-md py-2 mt-2 hover:bg-blue-800"
       >
-        <img
-          :src="`${product.img}`"
-          alt=""
-          class="w-48 h-48 mx-auto bg-gray-300 shadow-2xl rounded-lg"
-        />
-        <h2 class="font-bold text-xl pt-2 truncate ...">
-          {{ product.name }}
-        </h2>
-        <h4 class="text-gray-400">{{ album.price }}</h4>
-        <!-- <li v-for="album in albums" :key="album.test">
-          <h4 class="text-gray-400">{{ album.test }}</h4>
-        </li> -->
-      </div>
-    </router-link>
+        Add to Cart
+      </button>
+      <button
+        v-else
+        class="text-center w-full bg-gray-600 rounded-md py-2 mt-2 hover:cursor-default"
+      >
+        Sold Out
+      </button>
+    </div>
   </div>
 </template>
--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,40 +1,9 @@
 <script setup>
-import { onUnmounted, ref } from "vue";
-import productItem from "../composable/shopItem";
-
-const { products, unsubscribe } = productItem();
-
-onUnmounted(() => {
-  unsubscribe();
-});
+import ProductItem from "../components/ProductItem.vue";
 </script>
 
 <template>
   <div class="h-screen grid grid-cols-6 gap-x-8 pt-12">
-    <div v-for="product in products" :key="product.id">
-      <div
-        class="w-64 h-76 transition ease-in-out duration-500 p-4 rounded-md pb-6"
-      >
-        <img
-          :src="`${product.Img}`"
-          alt=""
-          class="w-56 h-56 mx-auto bg-gray-300 shadow-2xl"
-        />
-        <h2 class="font-bold text-xl pt-2 truncate ...">{{ product.Name }}</h2>
-        <h4 class="text-gray-400">${{ product.Price }}</h4>
-        <button
-          v-if="product.SoldOut == false"
-          class="text-center w-full bg-blue-600 rounded-md py-2 mt-2 hover:bg-blue-800"
-        >
-          Add to Cart
-        </button>
-        <button
-          v-else
-          class="text-center w-full bg-gray-600 rounded-md py-2 mt-2 hover:cursor-default"
-        >
-          Sold Out
-        </button>
-      </div>
-    </div>
+    <ProductItem />
   </div>
 </template>
